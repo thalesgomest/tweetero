78344b67b053ae2b5977fc69f74c7f568f4718ea
Thales Gomes Targino <thalestargino@gmail.com>
2022-04-25 16:59:43 -0300

    fix: inicial tweets conversation

--- index.js
+++ index.js
@@ -10,58 +10,58 @@ let users = [];
 let tweets = [
     {
         username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '100',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-58-639x700.jpg',
+        tweet: 'Hy guys, I am a cat',
+    },
+    {
+        username: 'Goku',
+        avatar: 'https://i.pinimg.com/originals/4d/86/5e/4d865ea47a8675d682ff35ad904a0af6.png',
+        tweet: 'I dont believe this, but I am a cat',
     },
     {
         username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '110',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-58-639x700.jpg',
+        tweet: 'Oh my god, seriously?',
+    },
+    {
+        username: 'Goku',
+        avatar: 'https://i.pinimg.com/originals/4d/86/5e/4d865ea47a8675d682ff35ad904a0af6.png',
+        tweet: 'Of course',
     },
     {
         username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '120',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-58-639x700.jpg',
+        tweet: 'Omg, we are the best cats ever',
+    },
+    {
+        username: 'Goku',
+        avatar: 'https://i.pinimg.com/originals/4d/86/5e/4d865ea47a8675d682ff35ad904a0af6.png',
+        tweet: 'Yes, we are the best',
     },
     {
         username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '130',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-58-639x700.jpg',
+        tweet: 'Goku, Dragon Ball Z is onde of the best anime ever',
+    },
+    {
+        username: 'Goku',
+        avatar: 'https://i.pinimg.com/originals/4d/86/5e/4d865ea47a8675d682ff35ad904a0af6.png',
+        tweet: 'Thanks Bob, I love Bob Sponge too!',
     },
     {
         username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '140',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-58-639x700.jpg',
+        tweet: 'What do you think about a crossover?',
+    },
+    {
+        username: 'Goku',
+        avatar: 'https://i.pinimg.com/originals/4d/86/5e/4d865ea47a8675d682ff35ad904a0af6.png',
+        tweet: 'Will be amazing!',
     },
     {
         username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '150',
-    },
-    {
-        username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '160',
-    },
-    {
-        username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '170',
-    },
-    {
-        username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '180',
-    },
-    {
-        username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '190',
-    },
-    {
-        username: 'bobesponja',
-        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info',
-        tweet: '200',
+        avatar: 'https://thypix.com/wp-content/uploads/2021/11/sponge-bob-profile-picture-thypix-58-639x700.jpg',
+        tweet: 'Yes, i think so too',
     },
 ];
 
@@ -157,71 +157,3 @@ app.post('/tweets', (req, res) => {
 app.listen(PORT, () => {
     console.log(chalk.bold.green('Server is running on port 5000'));
 });
-
-// ! <-- No bonus -->
-
-/* import express from 'express';
-import cors from 'cors';
-import chalk from 'chalk';
-const PORT = 5000;
-
-const app = express();
-app.use(cors());
-
-let users = [];
-let tweets = [];
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-
-app.post('/sign-up', (req, res) => {
-    if (req.body.username && req.body.avatar) {
-        const userRegister = users.find(
-            (user) => user.username === req.body.username
-        );
-        if (userRegister) {
-            res.status(400).send('User already exists');
-        } else {
-            users.push(req.body);
-            res.status(201).send('OK');
-        }
-    } else {
-        res.status(400).send('Missing username or avatar');
-    }
-});
-
-app.get('/tweets', (req, res) => {
-    if (tweets.length < 10) {
-        let aux = [];
-        for (let i = tweets.length - 1; i >= 0; i--) {
-            aux.push(tweets[i]);
-        }
-        res.status(200).send(aux);
-    } else {
-        let aux = [];
-        for (let i = tweets.length - 1; i >= tweets.length - 10; i--) {
-            aux.push(tweets[i]);
-        }
-        res.status(200).send(aux);
-    }
-});
-
-app.post('/tweets', (req, res) => {
-    if (req.body.username && req.body.tweet) {
-        let tweet = {
-            username: req.body.username,
-            tweet: req.body.tweet,
-        };
-        const user = users.find((user) => user.username === tweet.username);
-        tweet = { ...tweet, avatar: user.avatar };
-        tweets.push(tweet);
-        res.status(201).send('OK');
-    } else {
-        res.status(400).send('Missing username or tweet');
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(chalk.bold.green('Server is running on port 5000'));
-});
- */
